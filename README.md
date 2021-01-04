0f2f07d14d5fb0b2c4121ef45e7b2b4aba9aa88a
Jan Ritter <git@janrtr.de>
2021-01-05 00:12:17 +0100

    feat: project init with get state machine arn function

---
+++ README.md
@@ -0,0 +1,18 @@
+# AWS Lambda power tuning executor
+
+## Setup
+
+### Create venv
+```bash
+python3 -m venv env
+```
+
+### Activate venv
+```bash
+source ./env/bin/activate
+```
+
+### Install requirements
+```bash
+pip3 install -r requirements.txt
+```
---
+++ executor.py
@@ -0,0 +1,32 @@
+import argparse
+import boto3
+
+
+def getStateMachineArnByStackName(stackName):
+    client = boto3.client("cloudformation")
+
+    response = client.describe_stacks(StackName=stackName)
+
+    outputs = response.outputs
+    for output in outputs:
+        if output["OutputKey"] == "StateMachineARN":
+            return output["OutputValue"]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--stack-name",
+        type=str,
+        help="Name of the aws-lambda-power-tuning cloudformation stack",
+        default="lambda-power-tuning",
+    )
+
+    args = parser.parse_args()
+
+    print("Using stack-name:", args.stack_name)
+
+
+if __name__ == "__main__":
+    main()
