5b41b1c80b7889113fb64d927c80d6c6ed01d6ef
Jan Ritter <git@janrtr.de>
2021-01-05 23:03:30 +0100

    feat: added function to start step function execution

--- executor.py
+++ executor.py
@@ -12,6 +12,15 @@ def getStateMachineArnByStackName(stackName):
         if output["OutputKey"] == "StateMachineARN":
             return output["OutputValue"]
 
+def startStepFunctionsExecution(stateMachineARN, input):
+    client = boto3.client("stepfunctions")
+
+    response = client.start_execution(
+        stateMachineArn=stateMachineARN,
+        input=input
+    )
+
+    return response['executionArn']
 
 def main():
     parser = argparse.ArgumentParser()
@@ -26,6 +35,9 @@ def main():
     args = parser.parse_args()
 
     print("Using stack-name:", args.stack_name)
+
+    stateMachineARN = getStateMachineArnByStackName(args.stack_name)
+    print("StepFunctions stateMachine ARN:", stateMachineARN)
 
 
 if __name__ == "__main__":
