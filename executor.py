d7f36f4279f2525ed705691cb24bb3b23fd7914c
Jan Ritter <jan.ritter@trustedshops.de>
2021-01-06 10:57:35 +0100

    feat: added check for execution status with progessbar

--- executor.py
+++ executor.py
@@ -1,5 +1,8 @@
 import argparse
 import boto3
+import time
+import progressbar
+import json
 
 
 def getStateMachineArnByStackName(stackName):
@@ -7,7 +10,7 @@ def getStateMachineArnByStackName(stackName):
 
     response = client.describe_stacks(StackName=stackName)
 
-    outputs = response.outputs
+    outputs = response['Stacks'][0]['Outputs']
     for output in outputs:
         if output["OutputKey"] == "StateMachineARN":
             return output["OutputValue"]
@@ -20,7 +23,40 @@ def startStepFunctionsExecution(stateMachineARN, input):
         input=input
     )
 
-    return response['executionArn']
+    executionARN = response['executionArn']
+    print("Execution started with ARN:",executionARN)
+
+    return executionARN
+
+def checkExecutionStatus(executionARN):
+    client = boto3.client("stepfunctions")
+
+    bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
+    runningChecks = 0
+
+    while True:
+        response = client.describe_execution(
+            executionArn=executionARN
+        )
+
+        status = response['status']
+        
+        if (status == "RUNNING"):
+            runningChecks = runningChecks + 1
+            bar.update(runningChecks)
+            time.sleep(0.5)
+
+        elif (status == "FAILED"):
+            bar.finish()
+            print("The execution failed, check the Step Functions console for more information")
+            break
+
+        else:
+            bar.finish()
+            print("\n----------\nStatus:", status)
+            print("Execution output:")
+            print(json.dumps(json.loads(response['output']), indent=4))
+            break
 
 def main():
     parser = argparse.ArgumentParser()
@@ -32,13 +68,25 @@ def main():
         default="lambda-power-tuning",
     )
 
+    parser.add_argument(
+        "--config",
+        type=str,
+        help="File name of the config used for testing",
+        default="",
+    )
+
     args = parser.parse_args()
 
     print("Using stack-name:", args.stack_name)
 
     stateMachineARN = getStateMachineArnByStackName(args.stack_name)
-    print("StepFunctions stateMachine ARN:", stateMachineARN)
+    print("Step Functions state machine ARN:", stateMachineARN)
 
+    print("\n----------\nLoading config from: "+args.config)
+    configContent = open(args.config, 'r').read()
+
+    executionARN = startStepFunctionsExecution(stateMachineARN, configContent)
+    checkExecutionStatus(executionARN)
 
 if __name__ == "__main__":
     main()
